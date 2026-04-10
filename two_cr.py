fa45bc8b0cb243eab623e0139e23aac810ac3c55
Prasad <70509263+Prasad-Padwal@users.noreply.github.com>
2026-04-10 20:02:59 +0530

    Add 2 Candle Retracement module

---
+++ two_cr.py
@@ -0,0 +1,12 @@
+"""
+2 Candle Retracement module
+
+Detects Bullish and Bearish 2 Candle Retracement patterns.
+"""
+
+def bullish_2cr(data):
+    pass # Implementation here
+
+
+def bearish_2cr(data):
+    pass # Implementation here
